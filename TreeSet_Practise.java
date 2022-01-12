adb101e219cdc1ebe9d4d8aeb9027fa249d23b6a
Raushan Kumar Gupta <66469791+raushankumarg6@users.noreply.github.com>
2022-01-12 23:04:25 +0530

    Create TreeSet_Practise.java

---
+++ TreeSet_Practise.java
@@ -0,0 +1,46 @@
+// package com.company;
+
+import java.util.TreeSet;
+
+
+
+public class TreeSet_Practise {
+    public static void main(String[] args) {
+        // 1 .
+//        TreeSet t = new TreeSet();
+//        t.add("A");
+//        t.add("a");
+//        t.add("B");
+//        t.add("Z");
+//        t.add("L");
+//
+//        t.add(new Integer(10)); // ClassCastException
+//        t.add(null); // RE : NPE
+//
+//        System.out.println(t);
+//
+//        TreeSet t = new TreeSet();
+//        t.add(new StringBuffer("A"));
+//        t.add(new StringBuffer("Z"));
+//        t.add(new StringBuffer("L"));
+//        t.add(new StringBuffer("B"));
+//
+//        System.out.println(t);
+
+        // Compare
+//        System.out.println("A".compareTo("Z")); // -ve
+//        System.out.println("Z".compareTo("B")); // +ve
+//        System.out.println("A".compareTo("A")); // 0
+//        System.out.println("A".compareTo(null)); // NPE
+
+
+
+
+
+
+
+
+
+
+    }
+}
